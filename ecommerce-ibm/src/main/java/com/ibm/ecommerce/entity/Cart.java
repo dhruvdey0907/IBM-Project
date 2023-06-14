801a44ce3cb5f0b75442bb8696495a963e1a51af
dhruvdey0907 <dhruba.5250@gmail.com>
2023-06-15 01:26:55 +0530

    Error Solve

--- ecommerce-ibm/src/main/java/com/ibm/ecommerce/entity/Cart.java
+++ ecommerce-ibm/src/main/java/com/ibm/ecommerce/entity/Cart.java
@@ -1,48 +1,49 @@
 package com.ibm.ecommerce.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table
 public class Cart {
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer CardId;
-	@OneToOne
-	private Product product;
-	@OneToOne
-	private User user;
-	
-	public Cart() {
-		
-	}
-	public Integer getCardId() {
-		return CardId;
-	}
-	public void setCardId(Integer cardId) {
-		CardId = cardId;
-	}
-	public Product getProduct() {
-		return product;
-	}
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-	public User getUser() {
-		return user;
-	}
-	public Cart(Product product, User user) {
-		this.product = product;
-		this.user = user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer cartId;
+    @OneToOne
+    private Product product;
+    @OneToOne
+    private User user;
+
+    public Cart(){
+
+    }
+
+    public Cart(Product product, User user) {
+        this.product = product;
+        this.user = user;
+    }
+
+    public Integer getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Integer cartId) {
+        this.cartId = cartId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
