3d6f8d5a4f95709e576dce3afe60a3a5c84b81bb
dhruvdey0907 <dhruba.5250@gmail.com>
2023-06-15 13:54:52 +0530

    product update

--- ecommerce-ibm/src/main/java/com/ibm/ecommerce/controller/ProductController.java
+++ ecommerce-ibm/src/main/java/com/ibm/ecommerce/controller/ProductController.java
@@ -1,34 +1,27 @@
 package com.ibm.ecommerce.controller;
 
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-
 import com.ibm.ecommerce.entity.ImageModel;
 import com.ibm.ecommerce.entity.Product;
 import com.ibm.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class ProductController {
-	@Autowired 
-	private ProductService productService;
-	
-	@PreAuthorize("hasRole('Admin')")
-	@PostMapping(value = {"/addNewProduct"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+
+    @Autowired
+    private ProductService productService;
+
+    @PreAuthorize("hasRole('Admin')")
+    @PostMapping(value = {"/addNewProduct"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public Product addNewProduct(@RequestPart("product") Product product,
                                  @RequestPart("imageFile") MultipartFile[] file) {
         try {
@@ -41,8 +34,8 @@ public class ProductController {
         }
 
     }
-	
-	public Set<ImageModel> uploadImage(MultipartFile[] multipartFiles) throws IOException {
+
+    public Set<ImageModel> uploadImage(MultipartFile[] multipartFiles) throws IOException {
         Set<ImageModel> imageModels = new HashSet<>();
 
         for (MultipartFile file: multipartFiles) {
@@ -56,31 +49,30 @@ public class ProductController {
 
         return imageModels;
     }
-	@PreAuthorize("hasRole('Admin')")
-	@GetMapping({"/getAllProducts"})
-	public List<Product> getAllProducts(@RequestParam(defaultValue="0")int pageNumber,@RequestParam(defaultValue="")String searchKey){
-		List<Product> result= productService.getAllProducts(pageNumber,searchKey);
-		System.out.println("Result size is"+result.size());
-		return result;
-	}
-	
-	@GetMapping({"/getProductDetailsById/{productId}"})
-	public Product getProductDetailsById(@PathVariable("productId")Integer productId) {
-		return productService.getProductDetailsById(productId);
-		
-	}
-	@PreAuthorize("hasRole('Admin')")
-	@DeleteMapping({"/deleteProductDetails/{productId}"})
-	public void deleteProductDetails(@PathVariable("productId") Integer productId) {
-		productService.deleteProductDetails(productId);
-	}
-	
-	@PreAuthorize("hasRole('User')")
+
+    @GetMapping({"/getAllProducts"})
+    public List<Product> getAllProducts(@RequestParam(defaultValue = "0") int pageNumber,
+                                        @RequestParam(defaultValue = "") String searchKey) {
+        List<Product> result = productService.getAllProducts(pageNumber, searchKey);
+        System.out.println("Result size is "+ result.size());
+        return result;
+    }
+
+    @GetMapping({"/getProductDetailsById/{productId}"})
+    public Product getProductDetailsById(@PathVariable("productId") Integer productId) {
+        return productService.getProductDetailsById(productId);
+    }
+
+    @PreAuthorize("hasRole('Admin')")
+    @DeleteMapping({"/deleteProductDetails/{productId}"})
+    public void deleteProductDetails(@PathVariable("productId") Integer productId) {
+        productService.deleteProductDetails(productId);
+    }
+
+    @PreAuthorize("hasRole('User')")
     @GetMapping({"/getProductDetails/{isSingleProductCheckout}/{productId}"})
     public List<Product> getProductDetails(@PathVariable(name = "isSingleProductCheckout" ) boolean isSingleProductCheckout,
                                            @PathVariable(name = "productId")  Integer productId) {
         return productService.getProductDetails(isSingleProductCheckout, productId);
     }
-	
-
 }
--- ecommerce-ibm/src/main/java/com/ibm/ecommerce/service/CartService.java
+++ ecommerce-ibm/src/main/java/com/ibm/ecommerce/service/CartService.java
@@ -1,10 +1,5 @@
 package com.ibm.ecommerce.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.ibm.ecommerce.configuration.JwtRequestFilter;
 import com.ibm.ecommerce.dao.CartDao;
 import com.ibm.ecommerce.dao.ProductDao;
@@ -12,35 +7,56 @@ import com.ibm.ecommerce.dao.UserDao;
 import com.ibm.ecommerce.entity.Cart;
 import com.ibm.ecommerce.entity.Product;
 import com.ibm.ecommerce.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CartService {
-	@Autowired
-	private CartDao cartDao;
-	@Autowired
-	private ProductDao productDao;
-	@Autowired
-	private UserDao userDao;
-public Cart addToCart(Integer productId) {
-	Product product=productDao.findById(productId).get();
-	String username=JwtRequestFilter.CURRENT_USER;
-	User user=null;
-	if(username!=null) {
-		 user=userDao.findById(username).get();
-	}
-	if(product !=null && user!=null) {
-		Cart cart=new Cart(product,user);
-		cartDao.save(cart);
-	}
-	 user =userDao.findById(username).get();
-	if(product!=null) {
-		
-	}
-	return null;
-}
-public List<Cart> getCartDetails() {
-    String username = JwtRequestFilter.CURRENT_USER;
-    User user = userDao.findById(username).get();
-    return cartDao.findByUser(user);
-}
+
+    @Autowired
+    private CartDao cartDao;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    public void deleteCartItem(Integer cartId) {
+        cartDao.deleteById(cartId);
+    }
+
+    public Cart addToCart(Integer productId) {
+        Product product = productDao.findById(productId).get();
+
+        String username = JwtRequestFilter.CURRENT_USER;
+
+        User user = null;
+        if(username != null) {
+            user = userDao.findById(username).get();
+        }
+
+        List<Cart> cartList = cartDao.findByUser(user);
+        List<Cart> filteredList = cartList.stream().filter(x -> x.getProduct().getProductId() == productId).collect(Collectors.toList());
+
+        if(filteredList.size() > 0) {
+            return null;
+        }
+
+        if(product != null && user != null) {
+            Cart cart = new Cart(product, user);
+            return cartDao.save(cart);
+        }
+
+        return null;
+    }
+
+    public List<Cart> getCartDetails() {
+        String username = JwtRequestFilter.CURRENT_USER;
+        User user = userDao.findById(username).get();
+        return cartDao.findByUser(user);
+    }
 }
